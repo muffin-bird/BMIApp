22cd7c02c74d33312008ae68f38a8a02aaf1bd51
muffin-bird <muffinmen617@gmail.com>
2021-08-17 13:40:31 +0900

    Create BMIApp

---
+++ BMIApp/BMIApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  BMIApp
+//
+//  Created by muffin man on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        heightTextField.placeholder = "身長をcmで入力して下さい。"
+        weightTextField.placeholder = "体重をkgで入力して下さい。"
+    }
+
+    @IBAction func calcButtonAction(_ sender: Any) {
+        let doubleH = Double(heightTextField.text!)
+        let doubleW = Double(weightTextField.text!)
+        bmiLabel.text = "あなたのBMIは\(calculation(height: doubleH!, weight: doubleW!))です"
+
+    }
+    
+    func calculation(height: Double, weight: Double) -> String {
+        let h = height / 100
+        let w = weight
+        var result = w / (h * h)
+        result = floor(result * 10) / 10
+        
+        return result.description
+    }
+}
+
